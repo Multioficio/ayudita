e3c5bcaaadbb03d69fdca64eb8d888008ecdfe4a
Multioficio <146791930+Multioficio@users.noreply.github.com>
2023-10-20 00:17:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("keyup", 
+e=>{
+
+  
+if (e.target.matches("#buscador")){
+
+      
+if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").
+forEach(Producto =>{
+
+          
+Producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?Producto.classList.remove("filtro")
+            :Producto.classList.add("filtro")
+      })
+
+  }
+
+
+})
